03802be0338e935aa6dd68793350cd457d77e0fe
mail4karthickr@gmail.com <mail4karthickr@gmail.com>
2019-02-10 16:46:15 -0800

    first commit

---
+++ TwitterSplashScreenAnimation/SplashView.swift
@@ -0,0 +1,9 @@
+//
+//  SplashView.swift
+//  TwitterSplashScreenAnimation
+//
+//  Created by Karthick Ramasamy on 2/10/19.
+//  Copyright © 2019 Karthick Ramasamy. All rights reserved.
+//
+
+import Foundation
